00a438e606eb64e57b60f00dd9d6c460d6663edb
Timofey Arkhipov <54080963+TimofeyArkhipov@users.noreply.github.com>
2021-01-17 21:26:26 +0200

    Add files via upload

--- src/js/change_url.js
+++ src/js/change_url.js
@@ -2,6 +2,15 @@
 
 let knight = document.getElementById('knight-img-change');
 let castle = document.getElementById('castle-img-change');
+
+if (screen.width <850) {
+    knight.src='img/man2.png';
+    castle.src='img/Слой3.png';
+    castle.width='350';
+    castle.height='440';
+}
+
+
 window.addEventListener('resize', function () {
     if (screen.width <850) {
         knight.src='img/man2.png';
